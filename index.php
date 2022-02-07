db2b523e4c57a4b766690d8a0f7da1832ff7ea7a
mosaabmohamed <mosup132@gmail.com>
2022-02-07 18:30:22 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
-
 $app = new \Slim\Slim();
-
+Use Hcode\Model\User;
 $app->config('debug', true);
 
 $app->get('/', function() {
@@ -15,11 +14,36 @@ $app->get('/', function() {
 });
 $app->get('/admin', function() {
     
+    User::verifyLogin();
+
 	$page = new Hcode\PageAdmin();
 
 	$page->setTpl("index");
 
 });
+$app->get('/admin/login', function(){
+
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("login");
+});
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
+}); 
 
 $app->run();
 
